f311d402c3d51d8c66475a62c0cb4f7cdc68f28f
Bjorn Pardo <bjorn@xend.com.ph>
2014-04-07 12:15:24 -0600

    apr71215

---
+++ main.js
@@ -0,0 +1,14 @@
+var verbatim = prompt  ("Enter any word"); {
+	console.log("You entered " + verbatim);
+	console.log("There are " + verbatim.length + " characters in " + verbatim + ".");
+	console.log(verbatim.charAt(3) + " is the third charcter of " + verbatim + ".");
+	console.log("Lowercase: " + verbatim.toLowerCase());
+	console.log("Uppercase: " + verbatim.toUpperCase());
+	console.log("Example: " + "Your word is " + verbatim + ".");
+	console.log("Subword: " + verbatim.substring(1,4));
+
+
+
+}
+
+
